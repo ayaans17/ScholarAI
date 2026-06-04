3c2c600e2e828fd40f847f652a390103d962722d
17aya <17.ayaan@gmail.com>
2026-06-04 20:08:17 +0100

    updated logic to extract page text and page number from pdf

--- backend/ingestion/pdf_loader.py
+++ backend/ingestion/pdf_loader.py
@@ -3,10 +3,16 @@ import fitz
 def extract_text_from_pdf(pdf_path):
 
     doc = fitz.open(pdf_path)
+    print("Total pages in PDF:", len(doc))
 
     text = ""
+    pages = []
 
-    for page in doc:
-        text += page.get_text()
+    for page_num, page in enumerate(doc):
+        print(f"Page {page_num + 1} text length:", len(page.get_text()))
+        pages.append({
+            "text": page.get_text(),
+            "page": page_num + 1
+        })
 
-    return text
+    return pages
